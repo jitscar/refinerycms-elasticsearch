89cf426e19542289f9faacbdb84012e21f2db065
jitscar <jitscar@gmail.com>
2014-09-24 17:55:05 +0600

    update routes.rb to get method

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Refinery::Core::Engine.routes.draw do
 
   # Frontend routes
   namespace :elasticsearch, :path => '' do
-    match "/search", :to => 'search#show', :as => 'search'
+    match "/search", :to => 'search#show', :as => 'search', :via => [:get]
   end
 
 end
