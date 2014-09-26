818ca925a53aadaf01a4c40cc530a922135fc8c2
Yaroslav Agarkov <ya.agarkov@i-sys.ru>
2014-09-26 19:51:26 +0600

    добавил морфологию

--- app/decorators/models/refinery/page_decorator.rb
+++ app/decorators/models/refinery/page_decorator.rb
@@ -2,21 +2,23 @@ begin
   Refinery::Page.class_eval do
     include ::Refinery::Elasticsearch::Searchable
 
-    define_mapping do
-      {
+    @@mapping = {
         title: { type:'string', boost:2.0 },
         browser_title: { type:'string' },
         menu_title: { type:'string' },
         meta_description: { type:'string' },
         part: {
-          type:'object',
-          properties: {
-            body: { type:'string', analyzer:'snowball' }
-          }
+            type:'object',
+            properties: {
+                body: { type:'string', analyzer: (I18n.locale.eql?(:ru) ? 'russian_morphology' : 'snowball') }
+            }
         },
         created_at: { type:'date' },
         updated_at: { type:'date' }
-      }
+    }
+
+    define_mapping do
+      @@mapping
     end
 
     def to_index
