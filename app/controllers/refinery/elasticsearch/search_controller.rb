8b4ef84325fc06ffc122886f7906a1a7bf3b3ba9
Yaroslav Agarkov <ya.agarkov@i-sys.ru>
2014-10-03 19:10:16 +0600

    (115) добавил обработку строки поиска на предмет спец символов

--- app/controllers/refinery/elasticsearch/search_controller.rb
+++ app/controllers/refinery/elasticsearch/search_controller.rb
@@ -4,14 +4,35 @@ module Refinery
 
       # Display search results given the query supplied
       def show
-        @query = params[:q]
-        @results = Elasticsearch.search(params[:q], page:(params[:page] || '1').to_i)
+        @query = sanitize_query(params[:q])
+        @results = Elasticsearch.search(@query, page:(params[:page] || '1').to_i)
       rescue Faraday::ConnectionFailed
         flash[:alert] = 'Search engine is unavailable, please try again later'
       ensure
         @results ||= Results.new
         present(@page = Refinery::Page.find_by_link_url("/search"))
       end
+
+      def sanitize_query(str)
+        # Escape special characters
+        # http://lucene.apache.org/core/old_versioned_docs/versions/2_9_1/queryparsersyntax.html#Escaping Special Characters
+        escaped_characters = Regexp.escape('\\+-&|!(){}[]^~*?:')
+        str = str.gsub(/([#{escaped_characters}])/, '') # Paste it instead 2nd param if smth. wrong: \\\\\1
+
+        # AND, OR and NOT are used by lucene as logical operators. We need
+        # to escape them
+        ['AND', 'OR', 'NOT'].each do |word|
+          escaped_word = word.split('').map {|char| "\\#{char}" }.join('')
+          str = str.gsub(/\s*\b(#{word.upcase})\b\s*/, " #{escaped_word} ")
+        end
+
+        # Escape odd quotes
+        quote_count = str.count '"'
+        str = str.gsub(/(.*)"(.*)/, '\1\"\3') if quote_count % 2 == 1
+
+        str
+      end
+
     end
   end
 end
